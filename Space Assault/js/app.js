1d559afecfbc5ba2580764f6c63347b95eff77e2
Leonard Vid <emelman331@Gmail.com>
2018-05-26 15:04:48 +0300

    game develop part one

---
+++ Space Assault/js/app.js
@@ -0,0 +1,84 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+
+// The main game loop
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    requestAnimFrame(main);
+};
+
+function init() {
+    terrainPattern = ctx.createPattern(resources.get('img/terrain.png'), 'repeat');
+
+    document.getElementById('play-again').addEventListener('click', function() {
+        reset();
+    });
+
+    reset();
+    lastTime = Date.now();
+    main();
+}
+
+resources.load([
+    'img/sprites.png',
+    'img/terrain.png'
+]);
+resources.onReady(init);
+
+// Game state
+var player = {
+    pos: [0, 0],
+    sprite: new Sprite('img/sprites.png', [0, 0], [39, 39], 16, [0, 1])
+};
+
+var bullets = [];
+var enemies = [];
+var explosions = [];
+
+var lastFire = Date.now();
+var gameTime = 0;
+var isGameOver;
+var terrainPattern;
+
+// The score
+var score = 0;
+var scoreEl = document.getElementById('score');
+
+// Update game objects
+function update(dt) {
+    gameTime += dt;
+
+    handleInput(dt);
+    updateEntities(dt);
+
+    // It gets harder over time by adding enemies using this
+    // equation: 1-.993^gameTime
+    generateEnemys();
+
+    checkCollisions();
+
+    scoreEl.innerHTML = score;
+};
+
+function generateEnemys{
+    if(Math.random() < 1 - Math.pow(.993, gameTime)) {
+        enemies.push({
+            pos: [canvas.width,
+                  Math.random() * (canvas.height - 39)],
+            sprite: new Sprite('img/sprites.png', [0, 78], [80, 39],
+                               6, [0, 1, 2, 3, 2, 1])
+        });
+    }
+}
