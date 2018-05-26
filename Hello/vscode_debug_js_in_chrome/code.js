4a8ad74201927878b0c7236e2bf3484262854c55
Leonard Vid <emelman331@Gmail.com>
2018-05-26 07:52:38 +0300

    presentation page

---
+++ Hello/vscode_debug_js_in_chrome/code.js
@@ -0,0 +1,12 @@
+// поставьте тут brakepoint
+console.log('js started');
+
+function show()
+{
+    // и тут поставьте brakepoint
+    var greeting = 'Hello world';
+    console.log(greeting);
+    return alert(greeting);
+}
+
+console.log('js finished');
