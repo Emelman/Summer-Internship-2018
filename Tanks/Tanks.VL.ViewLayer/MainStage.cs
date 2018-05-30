a29b726fd00e3561e261a8bbc1814b459808fb98
Leonard Vid <emelman331@Gmail.com>
2018-05-30 09:40:52 +0300

    create tanks project

---
+++ Tanks/Tanks.VL.ViewLayer/MainStage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanks.VL.ViewLayer
+{
+    public partial class MainStage : Form
+    {
+        public MainStage(string[] args)
+        {
+            InitializeComponent();
+            this.Size = new Size(int.Parse(args[0]), int.Parse(args[1]));
+        }
+    }
+}
